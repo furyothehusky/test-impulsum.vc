01c00eff6589065b357620738bf330ae141cb54b
Alexis Pifano <apifanoviez@gmail.com>
2024-02-26 00:52:06 +0100

    added / as a special character in RegExp

--- src/helpers/regexp.tsx
+++ src/helpers/regexp.tsx
@@ -2,7 +2,7 @@ export const hasMinimumLength = (str: string, minLength: number) => str.length >
 export const hasNumber = (str: string) => /\d/.test(str);
 export const hasLowercase = (str: string) => /[a-z]/.test(str);
 export const hasUppercase = (str: string) => /[A-Z]/.test(str);
-export const hasSpecialChar = (str: string,) => /[!@#$%^&*(),.?":{}|<>]/.test(str);
+export const hasSpecialChar = (str: string,) => /[!@#$%^&*(),.?":{}|/<>]/.test(str);
 
 
 export const simulatePasswordChangeRequest = async (password: string): Promise<boolean> => {
